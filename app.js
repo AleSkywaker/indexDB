b500be81dc66444a534fdbb8e98202f195fd5876
Alex Colombo <uricaine@hotmail.com>
2019-03-16 11:08:27 +0100

    init indexDB

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ let request = window.indexedDB.open('mi-database', 1)
 
 //Se actualiza cuando se crea o se sube de version la DB
 
+request.onupgradeneeded = event =>{
+    console.log('Actualizacion de DB')
+}
+
