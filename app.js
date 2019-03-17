0356c72c087067d90b5b9da650851ca3e91ddc19
Alex Colombo <uricaine@hotmail.com>
2019-03-17 09:22:22 +0100

    managing errors with  indexDB

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-
 // indexedDB: Reforzamiento
 
-let request = window.indexedDB.open('mi-database', 2)
-
+let request = window.indexedDB.open('mi-database', 2);
 
 //Se actualiza cuando se crea o se sube de version la DB
 
-request.onupgradeneeded = event =>{
-    console.log('Actualizacion de DB')
+request.onupgradeneeded = (event) => {
+	console.log('Actualizacion de DB');
 
-    let db = event.target.result;
+	let db = event.target.result;
 
-    db.createObjectStore('heroes', {
-        keyPath: 'id'
-    })
-}
+	db.createObjectStore('heroes', {
+		keyPath: 'id'
+	});
+};
 
 //Manejo de errores
 
-request.onerror = event =>{
-    console.log("DB error : ", event.target.error)
-}
+request.onerror = (event) => {
+	console.log('DB error : ', event.target.error);
+};
+
+//insertar datos
+
+request.onsuccess = (event) => {
+    let db = event.target.result;
+};
